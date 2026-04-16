2325f265413eff8f3580811aa417d530085ea4b7
tpacompiap <tpacompiap@unjbg.edu.pe>
2026-04-15 21:41:26 -0500

    change collections to tips

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,17 +2,17 @@ import { defineCollection } from 'astro:content';
 import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
-const hello = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/hello' }),
+const tips = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/tips' }),
   schema: z.object({
     title: z.string(),
-    pubDate: z.string(),
+    pubDate: z.coerce.date(),
     description: z.string(),
     image: z.string().optional(),
-    tags:  z.array(z.string())
+    tags:  z.array(z.string()),
   }),
 });
 
 // Expose your defined collection to Astro
 // with the `collections` export
-export const collections = { hello };
+export const collections = { tips };
